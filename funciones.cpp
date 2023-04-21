7103cb6b901edd154c86fd75ad9b49cc25bfd534
jadg13 <jaduran@doc.uca.edu.ni>
2023-04-21 08:13:38 -0600

    Programa con menu

--- funciones.cpp
+++ funciones.cpp
@@ -2,26 +2,61 @@
 
 using namespace std;
 
-#define MAX 10
+#define MAX 100
 
-//Arreglo.
+// Arreglo.
 int edades[MAX];
-int pos=0; //posición del elemento
+int pos = 0; // posición del elemento
 
 void agregarEdad(int edad);
 bool hayEspacio();
+void mostrarEdades();
+bool esMayor(int edad);
+int menu();
 
-void agregarEdad(int edad){
+void agregarEdad(int edad)
+{
     edades[pos] = edad;
     pos++;
 }
 
-bool hayEspacio(){
+bool hayEspacio()
+{
     return pos < MAX;
 }
 
+void mostrarEdades()
+{
+    for (int i = 0; i < pos; i++)
+    {
+        cout << edades[i] << endl;
+    }
+}
 
+bool esMayor(int edad)
+{
+    return edad >= 18;
+}
 
+void mostrarEdadesMayores()
+{
+    for (int i = 0; i < pos; i++)
+    {
+        if (esMayor(edades[i]))
+            cout << edades[i] << endl;
+    }
+}
 
-
-
+int menu()
+{
+    int opcion;
+    system("cls");
+    cout << "Listado de Edades " << endl;
+    cout << "1. Agregar edad\n";
+    cout << "2. Mostrar edades\n";
+    cout << "3. Mostrar edades mayores o igual a 18\n";
+    cout << "4. Salir\n";
+    cout << "Digite la opcion: ";
+    cin >> opcion;
+    return opcion;
+}
--- principal.cpp
+++ principal.cpp
@@ -1,9 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include "funciones.cpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int cant, edad, i = 0, opcion=0;
+    do{
+        opcion = menu();
+        switch(opcion){
+            case 1:
+                cout << "Ingrese la edad: ";
+                cin >> edad;
+                agregarEdad(edad);
+                system("pause");
+                break;
+            case 2:
+                mostrarEdades();
+                system("pause");
+                break;
+            case 3:
+                mostrarEdadesMayores();
+                system("pause");
+                break;
+            case 4:
+                break;
+            default :
+                cout<< "Opcion invalida...\n";
+                system("pause");
+                break;
+        }
+    }while(opcion !=4);
     return 0;
+
+
 }
